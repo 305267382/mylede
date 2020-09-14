ee9114aa89783904a40c557b7452014629624ee0
305267382 <53901698+305267382@users.noreply.github.com>
2020-09-14 11:56:33 +0800

    20200914

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,11 +21,11 @@ sed -i '$a src-git targets https://github.com/openwrt/targets.git' feeds.conf.de
 sed -i '$a src-git management https://github.com/openwrt-management/packages.git' feeds.conf.default
 #sed -i '$a src-git oldpackages http://git.openwrt.org/packages.git' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/xiaoqingfengATGH/feeds-lienol.git' feeds.conf.default
-sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
-sed -i '$a src-git homeclash https://github.com/xiaoqingfengATGH/homeclash.git' feeds.conf.default
-sed -i '$a src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git' feeds.conf.default
+#sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/xiaoqingfengATGH/feeds-lienol.git' feeds.conf.default
+#sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+#sed -i '$a src-git homeclash https://github.com/xiaoqingfengATGH/homeclash.git' feeds.conf.default
+#sed -i '$a src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git' feeds.conf.default
 #sed -i '$a src-git free https://github.com/305267382/lede-packages.git' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
